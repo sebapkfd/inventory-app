55c87e9f52320cbb0f9a430c58f95d93a5009835
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-02-13 19:50:54 -0300

    Added manufacturer list view

--- controllers/manufacturerController.js
+++ controllers/manufacturerController.js
@@ -1,8 +1,14 @@
 var Manufacturer = require('../models/manufacturer');
 
 // Display list of all manufacturers.
-exports.manufacturer_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: manufacturer list');
+exports.manufacturer_list = function(req, res, next) {
+    
+    Manufacturer.find()
+    .sort('ascending')
+    .exec(function(err, list_manufacturers) {
+        if (err) { return next(err) }
+        res.render('manufacturer_list', {title: 'Manufacturer List', manufacturer_list: list_manufacturers });
+    });
 };
 
 // Display detail page for a specific manufacturer.
