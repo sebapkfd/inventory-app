3f3a7afe9958ff782eda229b7ea50e16db2ea9b2
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-02-16 18:44:25 -0300

    Commented key import

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var inventoryRouter = require('./routes/inventory');
 
-var {key} = require('./info')
+// var {key} = require('./info');
 var app = express();
 
 var mongoose = require('mongoose');
