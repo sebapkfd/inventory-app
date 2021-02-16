049d2302cd9fc4a05cc9e0c0db2558e5d7d25be5
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-02-16 18:28:28 -0300

    Preparing for deployment

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var {key} = require('./info')
 var app = express();
 
 var mongoose = require('mongoose');
-var mongoDB = key;
+var mongoDB = process.env.MONGODB_URI || key;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
